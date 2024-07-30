c194e399eee005baa968a77da9dbc404a5483c9b
Francisco Soto <francisco.soto@duhovit.cl>
2024-07-29 22:16:30 -0400

    Test

--- AprendiendoDjango/urls.py
+++ AprendiendoDjango/urls.py
@@ -13,6 +13,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    Esta es una prueba de las ramas xdd
 """
 from django.contrib import admin
 from django.urls import path, include
